8bd3225d6fe0a1aaeee1c3fed3a11c756069d725
Venkatesh Chakrabarty <venkatesh46.ch@gmail.com>
2021-04-19 02:58:18 +0530

    [UPDATE] Push changes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 mongoose.connect(`mongodb+srv://venkatesh:${process.env.PWD}@mycluster.fyzgz.mongodb.net/maya-data?retryWrites=true&w=majority`, 
 { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false}
